e7a1b0fcb24e2cf732de46c9a5fe7878f91755c2
17benabdallah-hue <17benabdallah@gmail.com>
2026-03-06 02:34:47 +0100

    Update layout.tsx
    Signed-off-by: 17benabdallah-hue <17benabdallah@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import { ErrorBoundary } from '../components/ErrorBoundary';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="ar">
-      <body>
+    <html lang="ar" suppressHydrationWarning>
+      <body className="bg-gray-100 text-gray-800 dark:bg-black dark:text-gray-200">
         <ErrorBoundary>
           <LanguageProvider>{children}</LanguageProvider>
         </ErrorBoundary>
